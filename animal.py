3c1c645c71a2baf5ec9c757e6d409053d01ff88a
GalinaDimitrova <galinadimitrova1993@gmail.com>
2014-11-09 17:05:15 +0200

    Make changes to functions: grow, eat and die; added function age in days in animal.py. Added tests to animal_class_tests.py

--- animal.py
+++ animal.py
@@ -1,6 +1,5 @@
 import random
 from zoo_database import ZooDatabase
-#from zoo import Zoo
 
 
 class Animal(ZooDatabase):
@@ -20,35 +19,52 @@ class Animal(ZooDatabase):
         self.meat_eaten = 0
         self.grass_eaten = 0
 
+    # def grow(self):
+    #     age_in_days = self.age * Animal.DAYS_IN_MONTH
+    #     age_in_days += 1
+    #     if self.weight < self.average_weight:
+    #         weight_age_ratio_per_day = self.weight_age_ratio / Animal.DAYS_IN_MONTH
+    #         self.weight += weight_age_ratio_per_day
+    #     if age_in_days % Animal.DAYS_IN_MONTH < 15:
+    #         self.age = age_in_days // 30
+    #     else:
+    #         self.age = (age_in_days // 30) + 1
+
     def grow(self):
-        age_in_days = self.age * Animal.DAYS_IN_MONTH
-        age_in_days += 1
+        day = 0.01
+        age_in_months = int(self.age)
+
         if self.weight < self.average_weight:
             weight_age_ratio_per_day = self.weight_age_ratio / Animal.DAYS_IN_MONTH
             self.weight += weight_age_ratio_per_day
-        if age_in_days % Animal.DAYS_IN_MONTH < 15:
-            self.age = age_in_days // 30
+
+        if self.age < age_in_months + 0.30:
+            self.age += day
         else:
-            self.age = (age_in_days // 30) + 1
+            self.age = int(self.age) + 1
 
-    def eat(self, food):
+    def eat(self):
         kg = self.weight * self.food_weight_ratio
-        self.weight += kg
-        if food == "meat" and self.food_type == food:
-            #Zoo().available_meat -= kg
+        kg = round(kg, 2)
+        self.grow()
 
+        if self.food_type == "meat":
             self.meat_eaten += kg
-        elif food == "grass" and self.food_type == food:
-            #Zoo().available_grass -= kg
+        else:
             self.grass_eaten += kg
 
-        else:
-            return "I don't eat {}. Give me other food.".format(food)
+    def age_in_days(self):
+        months_in_days = int(self.age) * Animal.DAYS_IN_MONTH
+        days_in_age = (self.age - int(self.age))* 100
+
+        age_in_months = months_in_days + round(days_in_age)
+        return age_in_months
 
     def die(self):
         life_expectancy_days = self.life_expectancy * Animal.DAYS_IN_YEAR
-        age_in_days = self.age * Animal.DAYS_IN_MONTH
+        age_in_days = self.age_in_days()
         chance_of_dying = age_in_days / life_expectancy_days
+
         random_number = random.random()
         if random_number > chance_of_dying:
             return True
--- animal_class_tests.py
+++ animal_class_tests.py
@@ -6,46 +6,77 @@ from animal import Animal
 class AnimalTests(unittest.TestCase):
 
     def setUp(self):
-        self.animal = Animal("tiger", 144, "DiviaLud", "male", 300)
+        self.animal = Animal("tiger", 144, "DiviaLud", "male", 120)
 
     def test_init(self):
         self.assertEqual(self.animal.species, "tiger")
         self.assertEqual(self.animal.age, 144)
         self.assertEqual(self.animal.name, "DiviaLud")
         self.assertEqual(self.animal.gender, "male")
-        self.assertEqual(self.animal.weight, 300)
+        self.assertEqual(self.animal.weight, 120)
+
+        self.assertEqual(self.animal.life_expectancy, 15)
+        self.assertEqual(self.animal.food_type, "meat")
+        self.assertEqual(self.animal.gestation_period, 3)
+        self.assertEqual(self.animal.newborn_weight, 1)
+        self.assertEqual(self.animal.average_weight, 250)
+        self.assertEqual(self.animal.weight_age_ratio, 3)
+        self.assertEqual(self.animal.food_weight_ratio, 0.07)
 
     def test_grow(self):
-        self.animal.grow(2, 50)
-        self.assertEqual(self.animal.age, 146)
-        self.assertEqual(self.animal.weight, 350)
+        self.animal.age = 10    # months
+        self.animal.grow()
+        self.assertEqual(self.animal.age, 10.01)
+        self.assertEqual(self.animal.weight, 120.1)
+
+    def test_grow_with_month(self):
+        self.animal.age = 10.30    # months
+        self.animal.grow()
+        self.assertEqual(self.animal.age, 11.00)
+        self.assertEqual(self.animal.weight, 120.1)
+
+    def test_grow_avarage_weight(self):
+        self.animal.weight = 250    # months
+        self.animal.grow()
+        self.assertEqual(self.animal.weight, 250)
 
     def test_eat_meat(self):
-        self.animal.eat("meat", 4)
-        self.assertGreater(self.animal.weight, 12)
-        self.assertEqual(self.animal.meat_eaten, 4)
-        #self.assertEqual(self.animal.Zoo.available_meat, 296)
+        self.animal.weight = 10
+        self.animal.age = 10
+        self.animal.eat()
+        self.assertEqual(self.animal.weight, 10.1)
+        self.assertEqual(self.animal.age, 10.01)
+        self.assertEqual(self.animal.meat_eaten, 0.7)
 
-        self.animal.eat("meat", 4)
-        self.assertGreater(self.animal.weight, 16)
-        self.assertEqual(self.animal.meat_eaten, 8)
-        #self.assertEqual(self.animal.Zoo.available_meat, 292)
+        self.animal.weight = 10
+        self.animal.eat()
+        self.assertEqual(self.animal.age, 10.02)
+        self.assertEqual(self.animal.meat_eaten, 1.4)
 
     def test_eat_grass(self):
-        self.animal.eat("grass", 4)
-        self.assertGreater(self.animal.weight, 12)
-        self.assertEqual(self.animal.grass_eaten, 4)
-        #self.assertEqual(self.animal.Zoo.available_grass, 196)
+        self.animal.food_type = "grass"
+        self.animal.weight = 10
+        self.animal.age = 10
+        self.animal.eat()
+        self.assertEqual(self.animal.weight, 10.1)
+        self.assertEqual(self.animal.age, 10.01)
+        self.assertEqual(self.animal.grass_eaten, 0.7)
 
-        self.animal.eat("grass", 4)
-        self.assertGreater(self.animal.weight, 16)
-        self.assertEqual(self.animal.grass_eaten, 8)
-        #self.assertEqual(self.animal.Zoo.available_grass, 192)
+        self.animal.weight = 10
+        self.animal.eat()
+        self.assertEqual(self.animal.age, 10.02)
+        self.assertEqual(self.animal.grass_eaten, 1.4)
+
+    def test_age_in_days(self):
+        self.animal.age = 1.01
+        self.assertEqual(self.animal.age_in_days(), 31)
+        self.animal.age = 10.10
+        self.assertEqual(self.animal.age_in_days(), 310)
 
     def test_die(self):
         died_or_not = []
         for i in range(100):
-            died_or_not.append(self.animal.die(180))
+            died_or_not.append(self.animal.die())
         self.assertIn(True, died_or_not)
         self.assertIn(False, died_or_not)
 
